892d626d2047f547491e797ae78bd8be4e0ac479
Himi <himishatunwal@gmail.com>
2023-05-15 21:44:31 +0530

    change

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const moongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength:6,
+    }
+})
+export default moongoose.model('User',userSchema)
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const { getAllUser } = require('../controllers/user-controller');
+const userRouter = express.Router();
+userRouter.get("/",getAllUser);
+export default userRouter;
