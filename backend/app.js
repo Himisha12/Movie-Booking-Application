f819abe04dd61f79e3e46218d537e49dd1b46bc2
Himi <himishatunwal@gmail.com>
2023-05-17 11:44:22 +0530

    user and admin

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ const app = express();
 const moongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.routes');
+const adminRouter = require('./routes/admin-routes');
+
 dotenv.config();
 
 //middleware
 app.use(express.json());
 
 app.use("/user",userRouter);
+app.use("/admin",adminRouter);
 
 moongoose.connect("mongodb+srv://himishatunwal:AklPcssDyVDVdnwu@movie-system.40tpy10.mongodb.net/?retryWrites=true&w=majority");
 
---
+++ backend/routes/admin-routes.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const adminRouter=express.Router();
+const {addAdmin
+    //,adminlogin,getAdmins, getAdminByID
+}=require('../controllers/admin-controller');
+
+adminRouter.post("/signup",addAdmin);
+// adminRouter.post("/login",adminlogin);
+// adminRouter.get('/',getAdmins);
+// adminRouter.get("/:id", getAdminByID);
+
+
+module.exports=adminRouter;
+
+// port chnaged 4000
