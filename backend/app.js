95ff7eebd01c67e8e010e9ecb4dcbb9406b15ca3
Himi <himishatunwal@gmail.com>
2023-05-17 20:03:57 +0530

    header and api

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,33 @@
 const express = require('express');
-
 const app = express();
-const moongoose = require('mongoose');
+const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-const userRouter = require('./routes/user.routes');
+const userRouter = require('./routes/user-routes');
 const adminRouter = require('./routes/admin-routes');
 const movieRouter = require('./routes/movie-routes');
+const bookingRouter = require('./routes/booking-routes');
 dotenv.config();
 
-//middleware
+const PORT = process.env.PORT || 4000
+
+const cors = require('cors');
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+// middleware section
 app.use(express.json());
+app.use("/users", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movies", movieRouter);
+app.use("/booking", bookingRouter);
 
-app.use("/user",userRouter);
-app.use("/admin",adminRouter);
-app.use("/movie",movieRouter);
+mongoose.connect(`${process.env.DATABASE}`)
 
-moongoose.connect("mongodb+srv://himishatunwal:AklPcssDyVDVdnwu@movie-system.40tpy10.mongodb.net/?retryWrites=true&w=majority");
-
-app.listen(3500, () => {
-
-    console.log("Connected $(3500)");
+app.listen(PORT, () => {
+    console.log(`Database connected.`);
 })
